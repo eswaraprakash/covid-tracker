4141ef0cef2fe8a0c500d6c3b5b2aa7a4b0493f5
EswaraPrakash Vaithiyanathan <v_eswara@live.com>
2021-05-09 17:03:16 +0530

    graph update

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -4,51 +4,26 @@ import numeral from "numeral";
 
 const options = {
   plugins:{
-    legend: {
-      display: false,
-    },
-  },
-  
-  elements: {
-    point: {
-      radius: 0,
-    },
-  },
-  maintainAspectRatio: false,
-  tooltips: {
-    mode: "index",
-    intersect: false,
-    callbacks: {
-      label: function (tooltipItem, data) {
-        return numeral(tooltipItem.value).format("+0,0");
+      legend: {
+        display: false
       },
+    elements: {
+      point: {
+        radius: 0
+      }
     },
-  },
-  scales: {
-    xAxes: [
-      {
-        type: "time",
-        time: {
-          format: "MM/DD/YY",
-          tooltipFormat: "ll",
-        },
-      },
-    ],
-    yAxes: [
-      {
-        gridLines: {
-          display: false,
-        },
-        ticks: {
-          // Include a dollar sign in the ticks
-          callback: function (value, index, values) {
-            return numeral(value).format("0a");
-          },
-        },
-      },
-    ],
-  },
-};
+    maintainAspectRatio: true,
+      tooltips: {
+        mode: "index",
+        intersect: false,
+        callbacks: {
+          label: function (tooltipItem, data) {
+            return numeral(tooltipItem.value).format("+0,0");
+          }
+        }
+      }
+    }
+  };
 
 const buildChartData = (data, casesType) => {
   let chartData = [];
